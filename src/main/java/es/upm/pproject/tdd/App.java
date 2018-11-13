82e3b4230bc8b56a9619b0075fa93fcfb36311b2
Víctor Nieves Sánchez <vnievess@gmail.com>
2018-11-13 11:57:16 +0100

    Fixed structure

---
+++ src/main/java/es/upm/pproject/tdd/App.java
@@ -0,0 +1,13 @@
+package es.upm.pproject.tdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
