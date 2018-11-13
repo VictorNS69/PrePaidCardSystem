0f5d41059653d7555cfa7b5fb3d53b1e3e2aea5c
Víctor Nieves Sánchez <vnievess@gmail.com>
2018-11-13 12:07:12 +0100

    updated to have 1 true test

--- src/test/java/es/upm/pproject/tdd/AppTest.java
+++ src/test/java/es/upm/pproject/tdd/AppTest.java
@@ -1,36 +1,17 @@
 package es.upm.pproject.tdd;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp()
     {
         assertTrue( true );
