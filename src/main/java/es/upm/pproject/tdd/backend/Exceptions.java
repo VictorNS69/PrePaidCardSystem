d04b7238cb140237b20523fda1c1d5a51340c0d4
Víctor Nieves Sánchez <victor.nieves.sanchez@alumnos.upm.es>
2018-11-30 11:03:27 +0100

    added new src

---
+++ src/main/java/es/upm/pproject/tdd/backend/Exceptions.java
@@ -0,0 +1,36 @@
+package es.upm.pproject.tdd.backend;
+
+class IncorrectPINException extends Exception{
+    private static final String MSG = "Error: Incorrect PIN.";
+    public IncorrectPINException(){
+        super(MSG);
+    }
+}
+
+class AlreadyRegisteredException extends Exception{
+	private static final String MSG = "Error: Card is alrready registered in the system.";
+	public AlreadyRegisteredException() {
+		super(MSG);
+	}
+}
+
+class NotRegisteredException extends Exception{
+    private static final String MSG = "Error: Card is not registered in the system.";
+    public NotRegisteredException(){
+        super(MSG);
+    }
+}
+
+class NotEnoughMoneyException extends Exception{
+    private static final String MSG = "Error: Card has not enough money.";
+    public NotEnoughMoneyException(){
+        super(MSG);
+    }
+}
+
+class ExpiredCardException extends Exception{
+    private static final String MSG = "Error: Card has expired.";
+    public ExpiredCardException(){
+        super(MSG);
+    }
+}
