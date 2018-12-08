def525173c01db26da4a704d042939f3d8e38e32
Víctor Nieves Sánchez <victor.nieves.sanchez@alumnos.upm.es>
2018-12-08 20:55:48 +0100

    Added test for BuyCard. Closes #9. There is a skipped test, that must try to create a card that ALREADY EXIST, but i really dont know how to test it without an infinite loop.

---
+++ src/test/java/es/upm/pproject/tdd/backend/BuyCardTest.java
@@ -0,0 +1,51 @@
+package es.upm.pproject.tdd.backend;
+
+import java.util.*;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BuyCardTest {
+	private Manager manager;
+	private List <Card> cardsList = new ArrayList<Card>();
+	private Card card;
+	
+	@BeforeEach
+	private void init() throws IncorrectPinFormatException, IncorrectPinException,
+	ExpiredCardException{
+		Card card = new Card(null, "Victor", "Nieves", "1234", 100, null);
+		this.cardsList.add(card);
+		this.manager = new Manager(this.cardsList);
+		this.card = this.manager.getCard(card.getNumber()); 
+	}
+	 
+	@Test
+	public void buyCardOk() throws IncorrectPinFormatException, IncorrectPinException, 
+	ExpiredCardException, AlreadyRegisteredException {
+		long number = this.manager.buyCard("Victor", "Nieves", "1234", 120);
+		this.cardsList.add(card);
+		assertNotNull(this.manager.getCard(number));
+	}
+	
+	@Disabled //how do we test this?
+	@Test
+	public void buyCardAlreadyExist() throws IncorrectPinFormatException, IncorrectPinException, 
+	ExpiredCardException, AlreadyRegisteredException {
+		long number = this.manager.buyCard("Victor", "Nieves", "1234", 100);
+		this.cardsList.add(card);
+		assertNotNull(this.manager.getCard(number));
+	}
+	
+	@Test
+	public void buyCardWrongPin_1() {
+		assertThrows(IncorrectPinFormatException.class, ()->{
+    		this.manager.buyCard("Daniel", "Morgera", "0", 0);
+		});
+	}
+	
+	@Test
+	public void buyCardWrongPin_2() {
+		assertThrows(IncorrectPinFormatException.class, ()->{
+    		this.manager.buyCard("Daniel", "Morgera", "1538458965", 0);
+		});
+	}
+}
