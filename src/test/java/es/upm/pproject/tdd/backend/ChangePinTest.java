146f11c3dcc5e515057107491a62ae049b5af48e
Victor Nieves Sanchez <victor.nieves.sanchez@alumnos.upm.es>
2018-12-28 13:55:18 +0100

    Closed #13

---
+++ src/test/java/es/upm/pproject/tdd/backend/ChangePinTest.java
@@ -0,0 +1,68 @@
+package es.upm.pproject.tdd.backend;
+
+import java.util.*;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import es.upm.pproject.tdd.exceptions.*;
+
+public class ChangePinTest {
+	private Manager manager;
+	private List <Card> cardsList = new ArrayList<Card>();
+	private Card card;
+	
+	@BeforeEach
+	private void init() throws IncorrectPinFormatException, IncorrectPinException,
+	ExpiredCardException{
+		String pin = new HashPin("1234").getHashPin();
+		Card card = new Card(null, "Victor", "Nieves", pin, 100, null);
+		this.cardsList.add(card);
+		this.manager = new Manager(this.cardsList);
+		this.card = this.manager.getCard(card.getNumber()); 
+	}
+	
+	@Test
+	public void changePinOk_1() throws IncorrectPinFormatException, 
+	NotRegisteredException, IncorrectPinException, ExpiredCardException {
+		String expectedPin = new HashPin("0001").getHashPin();
+		Card card = this.manager.changePIN(this.card.getNumber(), "1234", "0001");
+		assertEquals(expectedPin, card.getPin());
+	}
+	
+	@Test
+	public void changePinSamePin() throws IncorrectPinFormatException, 
+	NotRegisteredException, IncorrectPinException, ExpiredCardException {
+		String expectedPin = new HashPin("1234").getHashPin();
+		Card card = this.manager.changePIN(this.card.getNumber(), "1234", "1234");
+		assertEquals(expectedPin, card.getPin());
+	}
+	
+	@Test
+	public void changePinNullOldPin() throws IncorrectPinFormatException, 
+	NotRegisteredException, IncorrectPinException, ExpiredCardException {
+		assertThrows(IncorrectPinException.class, ()->{
+    		this.manager.changePIN(this.card.getNumber(), null, "0001");
+		});
+	}
+	
+	@Test
+	public void changePinNullNewPin() throws IncorrectPinFormatException, 
+	NotRegisteredException, IncorrectPinException, ExpiredCardException {
+		assertThrows(IncorrectPinException.class, ()->{
+    		this.manager.changePIN(this.card.getNumber(), "1234", null);
+		});
+	}
+	
+	@Test
+	public void changePinWrongPin_1() {
+		assertThrows(IncorrectPinFormatException.class, ()->{
+			this.manager.changePIN(this.card.getNumber(), "0", "0001");
+		});
+	}
+	
+	@Test
+	public void changePinWrongPin_2() {
+		assertThrows(IncorrectPinFormatException.class, ()->{
+			this.manager.changePIN(this.card.getNumber(), "123456732", "0001");
+		});
+	}
+}
