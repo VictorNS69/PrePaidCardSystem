71494728a6bf9cb638cf354a76f7badc3de6df89
Alejandro <alejandro.carmona.ayllon@alumnos.upm.es>
2018-12-30 04:50:20 -0800

    The window is now centered and all the functions that have to be done have been added

--- src/main/java/es/upm/pproject/tdd/frontend/MainFrontend.java
+++ src/main/java/es/upm/pproject/tdd/frontend/MainFrontend.java
@@ -14,20 +14,21 @@ import javax.swing.border.EmptyBorder;
 import es.upm.pproject.tdd.backend.*;
 import es.upm.pproject.tdd.exceptions.*;
 
-public class MainFrontend extends JFrame{
+public class MainFrontend extends JFrame {
 
-	//apartado carga de datos
+	// apartado carga de datos
 	Card card;
-	public final Path path = FileSystems.getDefault().getPath("src/assets/data.dat").toAbsolutePath();
-	Map<Long, Card> map = new HashMap<>();
+	private final Path path = FileSystems.getDefault().getPath("src/assets/data.dat").toAbsolutePath();
+	private Map<Long, Card> map = new HashMap<>();
 
-	//apartado botones y demas
+	// apartado botones y demas
 	private JPanel contentPanel;
-	//dejo los botones ya inicializados para asi solo tener que hacerlos visibles o no.
+	// dejo los botones ya inicializados para asi solo tener que hacerlos visibles o
+	// no.
 	private JButton pruebaDialog = new JButton("Dialog");
 	private JButton pruebaBeep = new JButton("Beep");
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 
 		EventQueue.invokeLater(new Runnable() {
 
@@ -35,90 +36,120 @@ public class MainFrontend extends JFrame{
 				try {
 					MainFrontend frame = new MainFrontend();
 					frame.setVisible(true);
-				}catch (Exception e) {
-					//esto hay que modificarlo, IMPORTANTE, si no se cambia por un logger
-					//el sonar dice que el proyecto no es evaluable
+				} catch (Exception e) {
+					// esto hay que modificarlo, IMPORTANTE, si no se cambia por un logger
+					// el sonar dice que el proyecto no es evaluable
 					e.printStackTrace();
 				}
 			}
-		}
-				);
+		});
 	}
 
-	//este main se va a dedicar exclusivamente a poner las settings de la ventana, cargar los datos
-	//cargar todos los botones y demas y dejarlos invisibles y una vez hecho eso, llamara a la funcion 
-	//MainMenu para que empiece el programa
-	public MainFrontend() throws ExpiredCardException, IOException, IncorrectPinException{
-		//titulo del frame
+	
+	/**
+	 * This main is going to be exclusively focused on putting settings on the window.
+	 * It will load the data, bring up the buttons and other elements in an invisible stay.
+	 * It will call the main function to initialize the aplication
+	 * @throws ExpiredCardException
+	 * @throws IOException
+	 * @throws IncorrectPinException
+	 */
+	public MainFrontend() throws ExpiredCardException, IOException, IncorrectPinException {
+		// titulo del frame
 		setTitle("PrePaidSystem");
-		//modo de salida del frame
+		// modo de salida del frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//posicion
-		setBounds(100,100,500,500);
-		//el contenedor
+		// tamanio
+		this.setSize(500, 500);
+		// centrado
+		Dimension window = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation((window.width / 2)-(this.getWidth() / 2),(window.height/2)-(this.getHeight()/2));
+		// el contenedor
 		contentPanel = new JPanel();
-		//pongo el panel en la ventana??
+		// pongo el panel en la ventana??
 		setContentPane(contentPanel);
 		contentPanel.setLayout(null);
-		//cargo el mapa
-		try{
+		// cargo el mapa
+		try {
 			map = new FileOperations().loadFile(path);
-		}
-		catch(ExpiredCardException | IOException | IncorrectPinException e) {
+		} catch (ExpiredCardException | IOException | IncorrectPinException e) {
 			e.printStackTrace();
 		}
-		//hacemos aqui los "add" de esta forma estan ya los botones (quiza haya que hacerlos invisibles todos segun empieza 
-		// porque haya un bug). el motivo de hacer esto aqui es que se buguea segun inicia el programa, y el primer boton
-		//lo pinta pero el segundo se queda oculto hasta que pasas el raton por encima. es un bug
+
+
+
+		// hacemos aqui los "add" de esta forma estan ya los botones (quiza haya que
+		// hacerlos invisibles todos segun empieza
+		// porque haya un bug). el motivo de hacer esto aqui es que se buguea segun
+		// inicia el programa, y el primer boton
+		// lo pinta pero el segundo se queda oculto hasta que pasas el raton por encima.
+		// es un bug
 		contentPanel.add(pruebaDialog);
 		contentPanel.add(pruebaBeep);
 		pruebaBeep.setVisible(false);
 		pruebaDialog.setVisible(false);
-		//llamo a la funcion donde va a empezar todo.
-		MainMenu();
-
-
+		// llamo a la funcion donde va a empezar todo.
+		Main();
 
 	}
 
-	//funcion donde empieza todo
-	public void MainMenu() {
-		//aparece el boton
+	// funcion donde empieza todo
+	public void Main() {
+		// aparece el boton
 		pruebaDialog.setVisible(true);
-		//posicion del boton
+		// posicion del boton
 		pruebaDialog.setBounds(100, 110, 100, 100);
-		//le doy la accion
-		pruebaDialog.addActionListener( new ActionListener() {
+		// le doy la accion
+		pruebaDialog.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//esta linea de codigo sirve para probar que el mapa se carga correctamente. resultado exitoso
-				//JOptionPane.showMessageDialog(contentPanel,map.values().toString());
-				//hago que cuando se pulse el boton, este se haga invisible
+				// esta linea de codigo sirve para probar que el mapa se carga correctamente.
+				// resultado exitoso
+				// JOptionPane.showMessageDialog(contentPanel,map.values().toString());
+				// hago que cuando se pulse el boton, este se haga invisible
 				pruebaDialog.setVisible(false);
-				//y llame a la funcion newButton
-				newButton();
+				// y llame a la funcion newButton
+				Pay();
 
-			}	
-		}
-				);
+			}
+		});
 	}
 
-	public void newButton () {
-		//esta funcion se encarga de pintar un nuevo boton en una posicion X (este boton tendra luego sus acciones)
+	public void Pay() {
+		// esta funcion se encarga de pintar un nuevo boton en una posicion X (este
+		// boton tendra luego sus acciones)
 		pruebaBeep.setVisible(true);
 		pruebaBeep.setBounds(10, 11, 100, 100);
-		pruebaBeep.addActionListener( new ActionListener() {
+		pruebaBeep.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Toolkit.getDefaultToolkit().beep();
-				//hago que cuando se pulse el boton, este se haga invisible
+				// hago que cuando se pulse el boton, este se haga invisible
 				pruebaBeep.setVisible(false);
-				//y llame a la funcion newButton
-				MainMenu();
+				// y llame a la funcion newButton
+				Main();
 
-			}	
-		}
-				);
+			}
+		});
 
 	}
-	//esto puede ser util porque podemos hacer un main bien hecho y desde ahi ir editando funciones que lleven 
-	//de una ventana del menu a otra y viceversa
+
+	public void BuyNewCard() {
+		// TODO
+	}
+
+	public void ChangePin() {
+		// TODO
+	}
+
+	public void ConsultBalance() {
+		// TODO
+	}
+
+	public void ConsultMovements() {
+		// TODO
+	}
+
+	public void ChargeMoney() {
+		// TODO
+	}
+
 }
